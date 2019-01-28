9915ac6f7a8915624bdde1b202ebfb6708f06ec4
HeXiHui <1501758282@qq.com>
2019-01-28 21:31:58 +0800

    1

--- git.js
+++ git.js
@@ -3,16 +3,14 @@
         
     git init 初始化 git文件
     git add .  提交所有文件
+    git status 查看文件状态
+    git commit -m'这儿写提交的注释'  // 这里提交之后 可以查找记录
+    git log  查看日志
 
-
-
-
-
-
-
-
-
-
+    git remote add origin https://github.com/HeXiHui/study.git //设置远程仓库
+     
+    git push origin 分支名 提交
+    git pull origin 分支名 拉取
 
 
 
